6e60bc3ceea6b466abff7e9cdd2df2f638f584b4
CodeByBrandon <CodeByBrandon.Programming@gmail.com>
2025-11-18 17:43:01 -0500

    Auto-commit: cambios detectados

--- app.py
+++ app.py
@@ -1097,7 +1097,17 @@ def historial_ventas():
     )
 
 
-
+@app.route('/ventas/eliminar/<int:id>', methods=['POST'])
+@login_required
+@role_required('admin')
+def eliminar_venta(id):
+    """Elimina una venta del historial"""
+    query = "DELETE FROM ventas WHERE id = %s"
+    ejecutar_query(query, (id,), commit=True)
+    
+    registrar_log('Venta eliminada', f'ID de venta eliminada: {id}')
+    flash('Venta eliminada correctamente.', 'success')
+    return redirect(url_for('historial_ventas'))
 
 
 # ---------------------------------------------------------------------------------
