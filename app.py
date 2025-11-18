c23bdffb8b8e666cb8a8d481612a37d841cd5694
CodeByBrandon <CodeByBrandon.Programming@gmail.com>
2025-11-18 13:51:41 -0500

    Auto-commit: cambios detectados

--- app.py
+++ app.py
@@ -1038,7 +1038,14 @@ def historial_ventas():
     fecha_hasta = request.args.get('fecha_hasta', '')
     
     
-    """
+    cursor.execute("""
+    SELECT v.id, v.fecha, v.hora, p.nombre, p.categoria, v.cantidad,
+           v.precio_unitario, v.iva, v.ganancia_unitaria
+    FROM ventas v
+    INNER JOIN productos p ON v.producto_id = p.id
+    ORDER BY v.id DESC
+""")
+
     
     params = []
     
