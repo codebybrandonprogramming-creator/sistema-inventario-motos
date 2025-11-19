a06311826a91094a8ad9d8b569c529d6d2f51fb3
CodeByBrandon <CodeByBrandon.Programming@gmail.com>
2025-11-18 20:35:36 -0500

    Auto-commit: cambios detectados

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 import pymysql
 from flask_wtf import CSRFProtect
-csrf = CSRFProtect(app)
+csrf = CSRFProtect()
 
 app = Flask(__name__)
 app.secret_key = "dev_secret_key_change_in_production"
